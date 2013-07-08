c1aa0ca610768c827f826005e7660d6e89545f2d
rvosa <rutgeraldo@gmail.com>
2013-07-08 18:11:27 +0200

    slightly farther along to creating a generic algorithm

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,3 @@
 cc -o helloworld `pkg-config --cflags --libs MagickWand` helloworld.c
-
+./helloworld ../data/img/out-of-sample/may07_mossii.png 50
 
--- src/helloworld.c
+++ src/helloworld.c
@@ -2,42 +2,120 @@
 #include <stdlib.h>
 #include <wand/MagickWand.h>
 
-int main(int argc,char **argv) {
-	PixelIterator *iterator;
-	MagickWand *wand;
-	PixelWand **row;
-	unsigned long width, height;
-	Quantum red, green, blue; // i.e. unsigned short
-  	
-	// set things up
-	MagickWandGenesis();
-	wand = NewMagickWand();
-		
-	// read an image, get dimensions
-	MagickReadImage(wand, argv[1]);
-	width = MagickGetImageWidth(wand);
-	height = MagickGetImageHeight(wand);
+// holds a set of values resulting from one of the phenotyping algorithms
+typedef struct PhenoType {
+	int size;
+	double *data;
+} PhenoType;
 
-	// iterate over rows
-	iterator = NewPixelIterator(wand);
-	for( int y = 0; y < height; y++ ) {
-		row = PixelGetNextIteratorRow(iterator,&width);
-		
-		// iterate over columns
-		for ( int x = 0; x < width; x++ ) {
-			
+// constructor for phenotype structs
+PhenoType *NewPhenoType(int bin_size) 
+{
+	PhenoType *result = (PhenoType*) malloc(sizeof(PhenoType));
+	double data[bin_size];
+	result->size = bin_size;
+	result->data = data;
+	return result;
+}
+
+// example algorithm
+PhenoType *average_pixel_value(MagickWand *wand, int bin_size) 
+{
+ 	PixelIterator *iterator = NewPixelIterator(wand);
+	Quantum red, green, blue; // i.e. unsigned short
+	PixelWand **row;
+	printf("initialized helper objects\n");
+	
+	// create a new phenotype struct
+	PhenoType *result = NewPhenoType(bin_size * 2);
+	printf("initialized PhenoType result data structure\n");
+	
+	// get image dimensions
+ 	unsigned long width = MagickGetImageWidth(wand);
+ 	unsigned long height = MagickGetImageHeight(wand);
+ 	printf("image width is %lu\n",width);
+ 	printf("image height is %lu\n",height);
+ 	
+ 	// this is how many pixels it takes until we've filled a bin over which to compute
+ 	int horiz_bin = width / bin_size;
+ 	int vert_bin  = height / bin_size;
+ 	printf("horizontal bin size is %d pixels\n", horiz_bin);
+ 	printf("vertical bin size is %d pixels\n", vert_bin);
+ 	
+ 	// will have a running tally over these
+ 	double x_average = 0.0;
+ 	double y_average = 0.0;
+ 	
+
+ 	// iterate over pixels
+ 	for ( int y = 0; y < height; y++ ) 
+ 	{
+		row = PixelGetNextIteratorRow(iterator,&width); 	
+ 		for ( int x  = 0; x < width; x++ ) 
+ 		{
+
 			// get RGB values
 			red   = PixelGetRedQuantum(row[x]);
 			green = PixelGetGreenQuantum(row[x]);
 			blue  = PixelGetBlueQuantum(row[x]);
 			
-			// print the output
-			printf("pixel %d,%d is rgb=(%d,%d,%d)\n", x, y, red, green, blue);
-		}
+			x_average = x_average + red + green + blue;
+			y_average = y_average + red + green + blue;
+			
+			if ( x % horiz_bin == 0 ) 
+			{
+				int x_idx = x / horiz_bin;
+				result->data[x_idx] = result->data[x_idx] + x_average;
+				x_average = 0.0;
+			}
+ 		}
+		if ( y % vert_bin == 0 ) 
+		{
+			int y_idx = ( y / vert_bin ) + bin_size;
+			printf("%d\n",y_idx);
+			result->data[y_idx] = result->data[y_idx] + y_average;
+			y_average = 0.0;
+		} 		
+ 	}
+ 	
+ 	// now divide
+ 	for ( int i = 0; i < bin_size; i++ ) 
+ 	{
+ 		result->data[i] = result->data[i] / 3 / horiz_bin;
+ 		result->data[i+bin_size] = result->data[i+bin_size] / 3 / vert_bin;
+ 		printf("bin %d, x value %f, y value %f\n", i, result->data[i], result->data[i+bin_size]); 		
+ 	}
+ 	
+	// clean up
+	iterator = DestroyPixelIterator(iterator); 	
+	return result;
+}
+
+int main(int argc,char **argv) 
+{
+	MagickWand *wand;
+  	
+	// set things up
+	MagickWandGenesis();
+	wand = NewMagickWand();
+	printf("initialized MagickWand\n");
+	
+	// read image
+	MagickReadImage(wand, argv[1]);
+	printf("read image %s\n", argv[1]);
+
+	// parse bin size
+	int bin_size = strtoul( argv[2], NULL, 10 );
+	printf("bin size %i\n", bin_size);
+	
+	// compute a phenotype
+	PhenoType *pt = average_pixel_value(wand,bin_size);
+	
+	for ( int i = 0; i < bin_size; i++ ) {
+		printf("%f\t%f\n", pt->data[i], pt->data[i+bin_size]);
 	}
 	
-	// clean up
-	iterator = DestroyPixelIterator(iterator);
+	// tear things down
 	wand = DestroyMagickWand(wand);
 	MagickCoreTerminus();
 	return(0);
